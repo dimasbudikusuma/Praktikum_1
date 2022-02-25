5bdfa1d410377ce3d30440009b57d787366ad349
Dimasbudikusuma <92243672+dimasbudikusuma@users.noreply.github.com>
2022-02-25 11:14:59 +0700

    Add files via upload

---
+++ Praktikum1/coba.php
@@ -0,0 +1,8 @@
+<?php
+$nama = "Dimas";
+$jenis_kelamin = "Lakik";
+$alamatsaya = "Jakarta";
+echo "Nama saya $nama";
+echo "<br> jenis kelamin saya $jenis_kelamin";
+echo "<br> alamat : $alamatsaya";
+?> 
